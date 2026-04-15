a0b3bebd05b7ac70b722d3b14361a3d3554f2e18
okantola25 <okantola25@students.oamk.fi>
2026-04-15 14:09:38 +0300

    profiili sivun postaus boxi

--- frontend/js/profile.js
+++ frontend/js/profile.js
@@ -1,3 +1,5 @@
+
+// tilin poisto nappi
 function showConfirmButtons() {
   document.getElementById("deleteBtn").classList.add("d-none");
   document.getElementById("confirmDeleteBtn").classList.remove("d-none");
@@ -8,4 +10,39 @@ function hideConfirmButtons() {
   document.getElementById("deleteBtn").classList.remove("d-none");
   document.getElementById("confirmDeleteBtn").classList.add("d-none");
   document.getElementById("cancelDeleteBtn").classList.add("d-none");
+}
+
+
+
+// omat postaukset
+const posts = [
+  {
+    title: "Ahven 1.2 kg",
+    text: "fafeferwg regerg gregerg hyfgfbrfthfb dfbdrg rewgsdfgerg  gerg eg erg erg geg"
+  },
+  {
+    title: "Kuha 2.8 kg",
+    text: "päpää   pälälälä pälääläopä"
+  },
+  {
+    title: "Hauki 4.1 kg",
+    text: "pläpäläpäläpälä "
+  }
+];
+
+let currentPostIndex = 0;
+
+function showPost() {
+  document.getElementById("postTitle").textContent = posts[currentPostIndex].title;
+  document.getElementById("postText").textContent = posts[currentPostIndex].text;
+}
+
+function nextPost() {
+  currentPostIndex = (currentPostIndex + 1) % posts.length;
+  showPost();
+}
+
+function previousPost() {
+  currentPostIndex = (currentPostIndex - 1 + posts.length) % posts.length;
+  showPost();
 }
